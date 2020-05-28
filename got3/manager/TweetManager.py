0d5a6aef0cf5a83cb480b5215b5f820c38418615
Anish Zute <31576491+anishzute@users.noreply.github.com>
2020-05-28 16:28:23 -0500

    error handling 2.0

--- got3/manager/TweetManager.py
+++ got3/manager/TweetManager.py
@@ -19,73 +19,75 @@ class TweetManager:
 
 		while active:
 			json = TweetManager.getJsonReponse(tweetCriteria, refreshCursor, cookieJar, proxy)
-			try:
-				len(json['items_html'].strip()) == 0
-			except:
-				break
-			if len(json['items_html'].strip()) == 0:
-				break
+			if not(json == 'error json'):
+				try:
+					len(json['items_html'].strip()) == 0
+				except:
+					break
+				if len(json['items_html'].strip()) == 0:
+					break
 			
 			
 
-			refreshCursor = json['min_position']
-			scrapedTweets = PyQuery(json['items_html'])
-			#Remove incomplete tweets withheld by Twitter Guidelines
-			scrapedTweets.remove('div.withheld-tweet')
-			tweets = scrapedTweets('div.js-stream-tweet')
+				refreshCursor = json['min_position']
+				scrapedTweets = PyQuery(json['items_html'])
+				#Remove incomplete tweets withheld by Twitter Guidelines
+				scrapedTweets.remove('div.withheld-tweet')
+				tweets = scrapedTweets('div.js-stream-tweet')
 			
-			if len(tweets) == 0:
-				break
-			
-			for tweetHTML in tweets:
-				tweetPQ = PyQuery(tweetHTML)
-				tweet = models.Tweet()
-				
-				usernameTweet = tweetPQ("span.username.js-action-profile-name b").text()
-				txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
-				retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
-				favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
-				dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"))
-				id = tweetPQ.attr("data-tweet-id")
-				permalink = tweetPQ.attr("data-permalink-path")
-				user_id = int(tweetPQ("a.js-user-profile-link").attr("data-user-id"))
-				
-				geo = ''
-				geoSpan = tweetPQ('span.Tweet-geo')
-				if len(geoSpan) > 0:
-					geo = geoSpan.attr('title')
-				urls = []
-				for link in tweetPQ("a"):
-					try:
-						urls.append((link.attrib["data-expanded-url"]))
-					except KeyError:
-						pass
-				tweet.id = id
-				tweet.permalink = 'https://twitter.com' + permalink
-				tweet.username = usernameTweet
-				
-				tweet.text = txt
-				tweet.date = datetime.datetime.fromtimestamp(dateSec)
-				tweet.formatted_date = datetime.datetime.fromtimestamp(dateSec).strftime("%a %b %d %X +0000 %Y")
-				tweet.retweets = retweets
-				tweet.favorites = favorites
-				tweet.mentions = " ".join(re.compile('(@\\w*)').findall(tweet.text))
-				tweet.hashtags = " ".join(re.compile('(#\\w*)').findall(tweet.text))
-				tweet.geo = geo
-				tweet.urls = ",".join(urls)
-				tweet.author_id = user_id
-				
-				results.append(tweet)
-				resultsAux.append(tweet)
-				
-				if receiveBuffer and len(resultsAux) >= bufferLength:
-					receiveBuffer(resultsAux)
-					resultsAux = []
-				
-				if tweetCriteria.maxTweets > 0 and len(results) >= tweetCriteria.maxTweets:
-					active = False
+				if len(tweets) == 0:
 					break
+			
+				for tweetHTML in tweets:
+					tweetPQ = PyQuery(tweetHTML)
+					tweet = models.Tweet()
+				
+					usernameTweet = tweetPQ("span.username.js-action-profile-name b").text()
+					txt = re.sub(r"\s+", " ", tweetPQ("p.js-tweet-text").text().replace('# ', '#').replace('@ ', '@'))
+					retweets = int(tweetPQ("span.ProfileTweet-action--retweet span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
+					favorites = int(tweetPQ("span.ProfileTweet-action--favorite span.ProfileTweet-actionCount").attr("data-tweet-stat-count").replace(",", ""))
+					dateSec = int(tweetPQ("small.time span.js-short-timestamp").attr("data-time"))
+					id = tweetPQ.attr("data-tweet-id")
+					permalink = tweetPQ.attr("data-permalink-path")
+					user_id = int(tweetPQ("a.js-user-profile-link").attr("data-user-id"))
+				
+					geo = ''
+					geoSpan = tweetPQ('span.Tweet-geo')
+					if len(geoSpan) > 0:
+						geo = geoSpan.attr('title')
+					urls = []
+					for link in tweetPQ("a"):
+						try:
+							urls.append((link.attrib["data-expanded-url"]))
+						except KeyError:
+							pass
+					tweet.id = id
+					tweet.permalink = 'https://twitter.com' + permalink
+					tweet.username = usernameTweet
+				
+					tweet.text = txt
+					tweet.date = datetime.datetime.fromtimestamp(dateSec)
+					tweet.formatted_date = datetime.datetime.fromtimestamp(dateSec).strftime("%a %b %d %X +0000 %Y")
+					tweet.retweets = retweets
+					tweet.favorites = favorites
+					tweet.mentions = " ".join(re.compile('(@\\w*)').findall(tweet.text))
+					tweet.hashtags = " ".join(re.compile('(#\\w*)').findall(tweet.text))
+					tweet.geo = geo
+					tweet.urls = ",".join(urls)
+					tweet.author_id = user_id
+				
+					results.append(tweet)
+					resultsAux.append(tweet)
 					
+					if receiveBuffer and len(resultsAux) >= bufferLength:
+						receiveBuffer(resultsAux)
+						resultsAux = []
+				
+					if tweetCriteria.maxTweets > 0 and len(results) >= tweetCriteria.maxTweets:
+						active = False
+						break
+				
+			
 		
 		if receiveBuffer and len(resultsAux) > 0:
 			receiveBuffer(resultsAux)
@@ -140,7 +142,7 @@ class TweetManager:
 			print("Twitter weird response. Try to see on browser: https://twitter.com/search?q=%s&src=typd" % urllib.parse.quote(urlGetData))
 			print("Unexpected error:", sys.exc_info()[0])
 # 			sys.exit()
-			return
+			return 'error json'
 		
 		dataJson = json.loads(jsonResponse.decode())
 		
